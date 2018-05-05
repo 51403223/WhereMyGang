c96277a34f318e261aa86feff45b8696a7ed04b6
Phat <huynhtanphat5596@gmail.com>
2018-05-05 13:02:18 +0700

    pha ne

--- app/src/main/java/com/logpht/wheremygang/MapActivity.java
+++ app/src/main/java/com/logpht/wheremygang/MapActivity.java
@@ -1,6 +1,9 @@
 package com.logpht.wheremygang;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.Fragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,7 +16,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -31,9 +39,11 @@ public class MapActivity extends AppCompatActivity
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng sydney = new LatLng(10.8691456, 106.7386724);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("My location"));
+        CameraUpdate cameraUpdate =CameraUpdateFactory.newLatLngZoom(sydney,15);
+        mMap.moveCamera(cameraUpdate);
+
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -106,11 +116,86 @@ public class MapActivity extends AppCompatActivity
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+            final Dialog createRoom = new Dialog(MapActivity.this);
+
+            createRoom.setTitle("Create Your Room");
+            createRoom.setContentView(R.layout.create_room);
+            final EditText nameRoom = createRoom.findViewById(R.id.nameRoom);
+            final EditText passRoom = createRoom.findViewById(R.id.passRoom);
+            Button btn_create = createRoom.findViewById(R.id.btn_createRoom);
+            btn_create.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(MapActivity.this, nameRoom.getText().toString()+"-" +
+                            passRoom.getText().toString(), Toast.LENGTH_SHORT).show();
+                    createRoom.dismiss();
+                }
+            });
+            createRoom.show();
+            Window window = createRoom.getWindow();
+            window.setLayout(Toolbar.LayoutParams.MATCH_PARENT, Toolbar.LayoutParams.WRAP_CONTENT);
+
         } else if (id == R.id.nav_gallery) {
+            final Dialog joinRoom = new Dialog(MapActivity.this);
+            joinRoom.setTitle("Join Room");
+            joinRoom.setContentView(R.layout.join_room);
+            final EditText idRoom = joinRoom.findViewById(R.id.id_room);
+            final EditText passRoom = joinRoom.findViewById(R.id.pass_join_room);
+            Button btn_join = joinRoom.findViewById(R.id.btn_joinRoom);
+            btn_join.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Toast.makeText(MapActivity.this, idRoom.getText().toString() + "-"
+                            + passRoom.getText().toString(), Toast.LENGTH_SHORT).show();
+                    joinRoom.dismiss();
+                }
+            });
+            joinRoom.show();
+            Window window = joinRoom.getWindow();
+            window.setLayout(Toolbar.LayoutParams.MATCH_PARENT, Toolbar.LayoutParams.WRAP_CONTENT);
 
         } else if (id == R.id.nav_slideshow) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MapActivity.this);
+            builder.setTitle("Leave Room");
+            builder.setMessage("Do you want to leave this Room ");
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    // neu no la chu phong ...........
+
+                    // neu no khong la chu phong .............
+                    Toast.makeText(MapActivity.this, "You left this Room", Toast.LENGTH_SHORT).show();
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(MapActivity.this, "Cancel", Toast.LENGTH_SHORT).show();
+                }
+            });
+            builder.show();
+
 
         } else if (id == R.id.nav_manage) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MapActivity.this);
+            builder.setTitle("Delte Room");
+            builder.setMessage("If you delete this Room, all member in Room will be dismissed. Are you sure to Delete Room?");
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+
+                    Toast.makeText(MapActivity.this, "Your Room is deleted", Toast.LENGTH_SHORT).show();
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Toast.makeText(MapActivity.this, "Cancel", Toast.LENGTH_SHORT).show();
+                }
+            });
+            builder.show();
+
+
 
         } else if (id == R.id.nav_share) {
 
